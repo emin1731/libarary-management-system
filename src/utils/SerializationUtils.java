3dd1513c663a01747b6150e222a5d00cdff41983
emin1731 <emin1731@icloud.com>
2024-05-03 17:50:36 +0400

    Add utils: SerializationUtils and TableCellListener

---
+++ src/utils/SerializationUtils.java
@@ -0,0 +1,41 @@
+package utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Base64;
+
+public class SerializationUtils {
+
+    // Helper method to serialize an object to a String
+    public static String serializeObjectToString(Object obj) {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
+            objectOutputStream.writeObject(obj);
+            objectOutputStream.flush();
+            return Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Helper method to deserialize an object from a String
+    @SuppressWarnings("unchecked")
+    public static <T> ArrayList<T> deserializeObjectFromString(String serializedString) {
+        byte[] bytes = Base64.getDecoder().decode(serializedString.trim());
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+             ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)) {
+
+            Object obj = objectInputStream.readObject();
+            return (ArrayList<T>) obj;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/utils/TableCellListener.java
@@ -0,0 +1,33 @@
+package utils;
+import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+import java.util.function.BiConsumer;
+
+public class TableCellListener implements TableModelListener {
+    private JTable table;
+    private BiConsumer<Integer, String> cellChangeListener; // Change Consumer to BiConsumer
+    private int columnIndex;
+
+    public TableCellListener(JTable table, int columnIndex, BiConsumer<Integer, String> cellChangeListener) { // Change Consumer to BiConsumer
+        this.table = table;
+        this.cellChangeListener = cellChangeListener;
+        this.table.getModel().addTableModelListener(this);
+        this.columnIndex = columnIndex;
+    }
+
+    @Override
+    public void tableChanged(TableModelEvent e) {
+        int row = e.getFirstRow();
+        int column = e.getColumn();
+
+        if (e.getType() == TableModelEvent.UPDATE && column == columnIndex) {
+            TableModel model = (TableModel) e.getSource();
+            String columnName = model.getColumnName(column);
+            String cellValue = String.valueOf(model.getValueAt(row, column));
+            System.out.println(columnName);
+            cellChangeListener.accept(row, cellValue);
+        }
+    }
+}
